00710c4f7759b2e925f9c06bebc81553206789c0
anitaKumari <anitadala583@gmail.com>
2024-07-29 12:16:15 +0530

    Adding Repositories

---
+++ src/main/java/com/project/uber/UberApp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.uber.UberApp.repository;
+
+import com.project.uber.UberApp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
